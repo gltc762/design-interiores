1acf5bad2e92e65301e71fed163cef802741e69e
Jediael Andreassa <jedi.andreassa@gmail.com>
2025-11-16 19:07:56 -0300

    include header

---
+++ includes/cabecalho.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inteo</title>
+    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header class="cabecalho">
+        <nav class="menu2">
+            <img src="images/logo.png" alt="inteo" class="logo">
+            <img src="images/logo2.png" alt="inteo" class="logo2">
+        </nav>
+        <nav class="menu">
+            <a href="#sobre" class="link-menu">Sobre</a>
+            <a href="#servicos" class="link-menu">Serviços</a>
+            <a href="#nosso-trabalho" class="link-menu">Nosso Trabalho</a>
+            <a href="#contato" class="link-menu">
+                <button class="botao-contato">CONTATO</button>
+            </a>
+        </nav>
+    </header>
--- index.php
+++ index.php
@@ -1,30 +1,8 @@
-<!DOCTYPE html>
-<html lang="pt-br">
+<?php
+include "includes/cabecalho.php";
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inteo</title>
-    <link rel="shortcut icon" href="images/favicon.png" type="image/x-icon">
-    <link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-    <header class="cabecalho">
-        <nav class="menu2">
-            <img src="images/logo.png" alt="inteo" class="logo">
-            <img src="images/logo2.png" alt="inteo" class="logo2">
-        </nav>
-        <nav class="menu">
-            <a href="#sobre" class="link-menu">Sobre</a>
-            <a href="#servicos" class="link-menu">Serviços</a>
-            <a href="#nosso-trabalho" class="link-menu">Nosso Trabalho</a>
-            <a href="#contato" class="link-menu">
-                <button class="botao-contato">CONTATO</button>
-            </a>
-        </nav>
-    </header>
-    <div class="content">
+<div class="content">
         <h1 class="titulo">Faça da sua casa uma ode à alegria</h1>
         <h4 class="texto">Transformamos sua casa vazia em um lar encantador, tornando espaços compactos <br> com móveis
             que
